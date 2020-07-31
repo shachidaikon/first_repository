99fca0910f04300e67137f63b4e3be2368c97259
shachidaikon <67863140+shachidaikon@users.noreply.github.com>
2020-07-31 20:25:41 +0900

    Update function.js

--- vegas/function.js
+++ vegas/function.js
@@ -2,12 +2,11 @@
 $(function() {
     $('body').vegas({
         slides: [
-            { src: 'sample01.jpg' },
-            { src: 'sample02.jpg' },
-            { src: 'sample03.jpg' },
-            { src: 'sample04.jpg' }
+            { src: 'animal crossing1.jpeg' },
+            { src: 'zelda.jpeg' },
+            { src: 'smabro.jpeg' },
+            { src: 'switch.jpg' }
         ],
-
         delay: 7000,
         timer: false,
         overlay: 'vegas/overlays/01.png',
